b4453c18bdf404a32958c6d4b73954fcb4ffebcb
gquesnot <gaqu1994@gmail.com>
2021-11-07 01:00:01 +0100

    ddza

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ echo "server {
 
 
         location / {
-            proxy_pass http://127.0.0.1:8000;
+            proxy_pass http://127.0.0.1:80;
             proxy_set_header X-Forwarded-Host \$server_name;
             proxy_set_header X-Real-IP \$remote_addr;
             proxy_redirect off;
