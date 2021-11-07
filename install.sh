befaab06c4fc8d2b65595ae8e039cdcbe27c3ef7
gquesnot <gaqu1994@gmail.com>
2021-11-07 01:29:41 +0100

    ddza

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ apt get update -y
 apt get install -y software-properties-common supervisor nginx vim libpq-dev python3 python-dev python3-dev \
      build-essential libssl-dev libffi-dev \
      libxml2-dev libxslt1-dev zlib1g-dev \
-     python-pip python3.9  python3.9-distutils
+     python-pip
 add-apt-repository ppa:deadsnakes/ppa
-apt install python3.9
+apt install -y python3.9 python3.9-distutils
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 10
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
